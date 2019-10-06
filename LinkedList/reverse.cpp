018525f5e4ebab45d4560592622087e54ffc9335
SURYANSH SINGH RATHORE <rathoresuryansh196@gmail.com>
2019-10-06 21:29:25 +0530

    Update reverse.cpp

--- LinkedList/reverse.cpp
+++ LinkedList/reverse.cpp
@@ -10,15 +10,15 @@
 */
 Node* Reverse(Node *head)
 {
-    Node *prev   = NULL;
-    Node *current = head;
-    Node *next;
-    while (current != NULL) {
-        next = current -> next;  
-        current -> next = prev;   
-        prev = current;
-        current = next;
+    Node *prev   = NULL;                        //pointer at previous node
+    Node *current = head;                       //pointer at current node
+    Node *next;                                 //pointer at next node
+    while (current != NULL) {                   //loop till current node is not NULL 
+        next = current -> next;                 //make 'next' pointer hold next node of current pointer
+        current -> next = prev;                 //change pointer direction
+        prev = current;                         //now make 'prev' pointer hold position of current
+        current = next;                         //now make 'current' pointer hold position of next
     }
-    head = prev;
-    return head;
+    head = prev;                                //setting head of new formed linked list
+    return head;                            
 }
